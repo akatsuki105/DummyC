61edf328d4b314625ecbf382de91ef274aa4fa55
akatsuki-py <bluejapan73+dev@gmail.com>
2020-01-18 14:52:32 +0900

    goインタプリタ本から不必要な部分を削除して移植

---
+++ pkg/parser/debug.go
@@ -0,0 +1,17 @@
+package parser
+
+// Errors - print parse error list
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
